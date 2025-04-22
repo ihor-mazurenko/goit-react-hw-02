d255d5ff7dd8dc5cc427a15aa32aae14a698b581
Ihor Mazurenko <igormazurenko94@gmail.com>
2025-04-22 23:53:21 +0300

    add all components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import Description from '../Description/Description'
+import Feedback from '../Feedback/Feedback'
+import Options from '../Options/Options'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <Description />
+      <Options />
+      <Feedback />
+    </>
+  )
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,11 @@
+export default function Feedback() {
+    return (
+        <>
+            <p>Good:</p>
+            <p>Neutral:</p>
+            <p>Bad:</p>
+            <p>Total:</p>
+            <p>Positiv:</p>
+        </>
+    )
+}
